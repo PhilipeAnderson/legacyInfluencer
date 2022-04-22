db7c7e1c995ab9bad36e835cca43e3af76c81751
philipe_kraken <philipeacampos@hotmail.com>
2022-04-22 10:20:11 -0300

    Configured colors and fonts from app

---
+++ frontend/src/theme/theme.ts
@@ -0,0 +1,19 @@
+export const theme = {
+  colors: {
+    backgroundDark: '#000000',
+    backgroundLight: '#1DCDB8',
+
+    enable: '#1DCDB8',
+    disable: '#C5C5C5',
+    text: '#FFFFFF',
+
+    success: '#1FB25A',
+    failure: '#F51355',
+    highlight: '#FFF500'
+  },
+  fonts: {
+    regular: 'Roboto_400Regular', 
+    medium: 'Roboto_500Medium', 
+    bold: 'Roboto_700Bold'
+  }
+}
