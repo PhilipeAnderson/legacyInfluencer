68d3bc9648b121408fd0d2c4b85bd746c37f77c2
philipe_kraken <philipeacampos@hotmail.com>
2022-05-04 23:24:16 -0300

    building HightLightCard component

--- frontend/src/components/HighlightCard/styles.ts
+++ frontend/src/components/HighlightCard/styles.ts
@@ -10,6 +10,7 @@ export const Content = styled.View`
   width: 200px;
   height: 300px;
   border-radius: 12px;
+  align-items: center;
 `;
 
 export const Image = styled.Image`
@@ -19,5 +20,17 @@ export const Image = styled.Image`
 `;
 
 export const InfoSquare = styled.View`
-  
+  position: absolute;
+  margin-top: 200px;
+  width: 90%;
+  height: 90px;
+  border-radius: 10px;
+  background-color: rgba(0,0,0,0.8);
+`;
+
+export const QRCode = styled.Image`
+
+  width: 50px;
+  height: 50px;
+  border-radius: 5px;
 `;
