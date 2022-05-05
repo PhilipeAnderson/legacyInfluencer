0cf583b359deae4442c25eb1a736f842e0703972
philipe_kraken <philipeacampos@hotmail.com>
2022-05-04 23:24:28 -0300

    refactored

--- frontend/src/components/HighlightCard/index.tsx
+++ frontend/src/components/HighlightCard/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 
-import { Container, Content, Image, InfoSquare } from './styles';
+import { Container, Content, Image, InfoSquare, QRCode } from './styles';
 
 export const HighlightCard = () => {
   return (
     <Container>
       <Content>
         <Image source={require('../../assets/model01.jpg')}/>
-        <InfoSquare />
+        <InfoSquare>
+          <QRCode source={require('../../assets/qrcode.png')}/>
+        </InfoSquare>
       </Content>
         
 
