27a59a06f3e223c5e0ecb5758cdd06b7fdd4f6b6
philipe_kraken <philipeacampos@hotmail.com>
2022-06-09 21:57:00 -0300

    Created the new page Contract

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -8,12 +8,13 @@ import { Login } from './src/screens/Login';
 import { Home } from './src/screens/Home';
 import { Confirm } from './src/screens/Confirm';
 import { Payment } from './src/screens/Payment';
+import { Contract } from './src/screens/Contract';
 
 
 export default function App() {
   return (
     <ThemeProvider theme={ theme }>
-      <Payment />
+      <Contract />
     </ThemeProvider>
   );
 }
