13f32c26ce5e921f1e4d3bbc32a35b1daefae6d9
philipe_kraken <philipeacampos@hotmail.com>
2022-06-14 23:27:33 -0300

    Created rating with stars in Profile influencer

--- frontend/src/screens/ProfileInfluencer/index.tsx
+++ frontend/src/screens/ProfileInfluencer/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Header } from '../../components/Header';
 
@@ -15,8 +15,45 @@ import {
   TopDataInfluencerStars
 } from './styles';
 
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
+
 export const ProfileInfluencer = () => {
-  return(
+
+  const [defaultRating, setDefaultRating] = useState(2);
+  const [maxRating, setMaxRating] = useState([1, 2, 3, 4, 5]);
+
+  const RatingBar = () => {
+    return (
+      <View style={styles.ratingBarStyle}>
+        {maxRating.map((item, key) => {
+          return (
+            <TouchableOpacity
+              activeOpacity={0.7}
+              key={item}
+              onPress={() => setDefaultRating(item)}>
+              <Image
+                style={styles.starImageStyle}
+                source={
+                  item <= defaultRating
+                    ? require('../../assets/starFull.png')
+                    : require('../../assets/starEmpty.png')
+                }
+              />
+            </TouchableOpacity>
+          );
+        })}
+      </View>
+    );
+  };
+
+  return (
     <Container>
       <Header title="Perfil Influencer" />
       <Content>
@@ -27,12 +64,68 @@ export const ProfileInfluencer = () => {
               <TopDataInfluencerName>Nathalia Amaral</TopDataInfluencerName>
               <TopDataInfluencerCity>Cidade: Rio De Janeiro</TopDataInfluencerCity>
               <TopDataInfluencerSegment>Segmento: Alimentação</TopDataInfluencerSegment>
-              <TopDataInfluencerStars> * * * * * </TopDataInfluencerStars>
+              {/* <TopDataInfluencerStars> * * * * * </TopDataInfluencerStars> */}
+              <SafeAreaView style={styles.containerStar}>
+                <View style={styles.containerStar}>
+                  <Text style={styles.textStyle}>
+                    Classifique a sua experiência
+                  </Text>
+                  <RatingBar />
+                  <Text style={styles.textStyle}>
+                    {defaultRating} / {Math.max.apply(null, maxRating)}
+                  </Text>
+                  <TouchableOpacity
+                    activeOpacity={0.7}
+                    style={styles.buttonStyle}
+                    onPress={() => alert(defaultRating)}>
+                    <Text style={styles.buttonTextStyle}>
+                      Obter Valor Selecionado
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+              </SafeAreaView>
             </TopDataInfluencerStarring>
           </TopDataInfluencer>
-          
+
         </DataInfluencer>
       </Content>
     </Container>
   )
 }
+
+const styles = StyleSheet.create({
+  containerStar: {
+    flex: 1,
+    backgroundColor: 'white',
+    padding: 10,
+    justifyContent: 'center',
+    textAlign: 'center',
+  },
+  textStyle: {
+    textAlign: 'center',
+    fontSize: 23,
+    color: '#000',
+    marginTop: 15,
+  },
+  buttonStyle: {
+    justifyContent: 'center',
+    flexDirection: 'row',
+    marginTop: 30,
+    padding: 15,
+    backgroundColor: '#8ad24e',
+  },
+  buttonTextStyle: {
+    color: '#fff',
+    textAlign: 'center',
+  },
+  ratingBarStyle: {
+    justifyContent: 'center',
+    flexDirection: 'row',
+    marginTop: 30,
+  },
+  starImageStyle: {
+    width: 40,
+    height: 40,
+    resizeMode: 'cover',
+  },
+});
