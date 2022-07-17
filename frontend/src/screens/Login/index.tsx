4bc02b3288f414e6581d6e4c4852296a9b83a9ed
philipe_kraken <philipeacampos@hotmail.com>
2022-07-17 10:51:32 -0300

    Set new configurations about Auth Session with Social Google

--- frontend/src/screens/Login/index.tsx
+++ frontend/src/screens/Login/index.tsx
@@ -1,18 +1,30 @@
 import React from 'react';
+import * as AuthSession from 'expo-auth-session';
 import { SignInButton } from '../../components/SignInButton';
 
 import GoogleSvg from '../../assets/icon-google.svg';
 
-import { 
-  Container, 
-  Header, 
-  Content, 
-  Logo, 
-  TextContent, 
-  TextLogin 
+import {
+  Container,
+  Header,
+  Content,
+  Logo,
+  TextContent,
+  TextLogin
 } from './styles';
 
 export const Login = () => {
+
+  async function handleSignIn() {
+    const CLIENT_ID = '1079107334277-nemhjhca87eup937lfjq2u7nno33h5sk.apps.googleusercontent.com';
+    const REDIRECT_URI = 'https://auth.expo.io/@philipeacampos/frontend';
+    const RESPONSE_TYPE = 'token';
+    const SCOPE = encodeURI('profile email');
+
+    const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
+    const response = await AuthSession.startAsync({ authUrl });
+    console.log(response);
+  }
 
   return (
     <Container>
@@ -34,6 +46,7 @@ export const Login = () => {
           title="Entrar com o Google"
           navig="Home"
           svg={GoogleSvg}
+          onPress={handleSignIn}
         />
       </Content>
     </Container>
